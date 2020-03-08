c2a28f783c156f9a8d44db7d088fc30d23f0765e
Pascal How <pascalhow@gmail.com>
2020-03-08 16:31:30 +0000

    Step 2 - Displaying a cookie

--- app/src/main/java/com/pascalhow/krumble/MainActivity.kt
+++ app/src/main/java/com/pascalhow/krumble/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.pascalhow.krumble
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var imgCookie: ImageView = findViewById(R.id.imgCookie)
+        var lblName: TextView = findViewById(R.id.lblName)
+        var lblStory: TextView = findViewById(R.id.lblStory)
+
+        imgCookie.setImageResource(R.drawable.cookie_image)
+        lblName.text=  "Mr Peanut"
+        lblStory.text = "Loves taking long walks on the beach"
     }
 }
